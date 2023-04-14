19f36071ec065862966148be0144a16f9cc816c6
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-04-14 18:36:58 -0300

    ready to play

---
+++ index.js
@@ -0,0 +1,76 @@
+//Detecting Button Press
+for (let i=0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+        let buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+
+        buttonAnimation(buttonInnerHtml);
+    });
+}
+
+//Detecting Keyboard Press
+document.addEventListener("keydown", (e) => {
+    makeSound(e.key);
+    buttonAnimation(e.key);
+})
+
+        function makeSound(key) {
+            switch (key) {
+            case "w":
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "a":
+                let kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+
+            case "s":
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+
+            case "d":
+                let tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "j":
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "k":
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+            case "l":
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            default: console.log(buttonInnerHtml);
+            }
+        }
+
+
+        function buttonAnimation(currentKey) {
+            let activeButton = document.querySelector("."+currentKey);
+            activeButton.classList.add("pressed");
+
+            setTimeout(() => {
+                activeButton.classList.remove("pressed");
+            }, 100);
+        }
+        
+    
+
+
+
+
+
+
+
